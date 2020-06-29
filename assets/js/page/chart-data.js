89c09ca7f758e395be10aedab4785439dd755581
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2020-06-29 17:24:12 +0700

    dashboard update minus chart

--- assets/js/page/chart-data.js
+++ assets/js/page/chart-data.js
@@ -1,4 +1,4 @@
-var ctx = document.getElementById('chartDashboardPraktikan');
+var ctx = document.getElementById('chartDashboardPraktikan').getContext('2d');
 var chartDashboardPraktikan = new Chart(ctx, {
     type: 'line',
     data: {
@@ -35,3 +35,81 @@ var chartDashboardPraktikan = new Chart(ctx, {
         }
     }
 });
+
+var ctx = document.getElementById('chartStrukdat').getContext('2d');
+var chartStrukdat = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Modul 1', 'Modul 2', 'Modul 3', 'Modul 4', 'Modul 5', 'Modul 6'],
+        datasets: [{
+            label: 'Nilai Akhir',
+            data: [90, 76, 99, 78, 90, 100],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById('chartAlpro').getContext('2d');
+var chartAlpro = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Modul 1', 'Modul 2', 'Modul 3', 'Modul 4', 'Modul 5', 'Modul 6'],
+        datasets: [{
+            label: 'Nilai Akhir',
+            data: [90, 76, 99, 78, 90, 100],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById('chartBasdat').getContext('2d');
